5be1fea47df2557848bfa86636db81abe8339c4f
Yusuf <fuzzy.repeal-0v@icloud.com>
2024-06-20 16:16:28 +0300

    exercise 1.6

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -1,39 +1,42 @@
 import { useState } from 'react'
 
-const Display = ({text}) => <h1>{text}</h1>  
-const Display2 = ({text}) => <h2>{text}</h2>  
-
-
+const Display = ({ text }) => <h1>{text}</h1>
+const Display2 = ({ text }) => <h2>{text}</h2>
+const Statistic = ({ text, value }) => <div>{text} {value}</div>
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const text = 'give feedback'
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
 
-  const increaseGood = () => {
-    setGood(good + 1)
+  const handleGoodClick = () => {
+    setFeedback({ ...feedback, good: feedback.good + 1 })
   }
 
-  const increaseNeutral = () => { 
-    setNeutral(neutral + 1)
+  const handleNeutralClick = () => {
+    setFeedback(
+      {
+        ...feedback, 
+        neutral: feedback.neutral + 1 
+      }
+    )
   }
 
-  const increaseBad = () => { 
-    setBad(bad + 1)
+  const handleBadClick = () => {
+    setFeedback({ ...feedback, bad: feedback.bad + 1 })
   }
 
   return (
     <div>
-      <Display text={text} />
-      <Button onClick={increaseGood} text="good" />
-      <Button onClick={increaseNeutral} text="neutral" />
-      <Button onClick={increaseBad} text="bad" />
+      <Display text="give feedback" />
+      <Button onClick={handleGoodClick} text="good" />
+      <Button onClick={handleNeutralClick} text="neutral" />
+      <Button onClick={handleBadClick} text="bad" />
       <Display2 text="statistics" />
+      <Statistic text="good" value={feedback.good} />
+      <Statistic text="neutral" value={feedback.neutral} />
+      <Statistic text="bad" value={feedback.bad} />
     </div>
   )
+}
 
-} 
-
-export default App
+export default App;
