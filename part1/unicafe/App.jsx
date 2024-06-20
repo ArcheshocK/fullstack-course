e7834dc37a2182dcc2226574f6733ee9f3a67363
Yusuf <fuzzy.repeal-0v@icloud.com>
2024-06-20 16:06:06 +0300

    some trial for exercise 1.6

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from 'react'
 
+const Display = ({text}) => <h1>{text}</h1>  
+const Display2 = ({text}) => <h2>{text}</h2>  
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const text = 'give feedback'
+
+  const increaseGood = () => {
+    setGood(good + 1)
+  }
+
+  const increaseNeutral = () => { 
+    setNeutral(neutral + 1)
+  }
+
+  const increaseBad = () => { 
+    setBad(bad + 1)
+  }
 
   return (
     <div>
-      code here
+      <Display text={text} />
+      <Button onClick={increaseGood} text="good" />
+      <Button onClick={increaseNeutral} text="neutral" />
+      <Button onClick={increaseBad} text="bad" />
+      <Display2 text="statistics" />
     </div>
   )
-}
+
+} 
 
 export default App
