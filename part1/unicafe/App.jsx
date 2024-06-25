f68965eb5233a4c7705304c93a4f6d3e17c20e96
Yusuf <fuzzy.repeal-0v@icloud.com>
2024-06-25 14:28:44 +0300

    exercise 1.7 submission

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -6,24 +6,47 @@ const Statistic = ({ text, value }) => <div>{text} {value}</div>
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 })
-
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0, total: 0})
+  
   const handleGoodClick = () => {
-    setFeedback({ ...feedback, good: feedback.good + 1 })
+    setFeedback({ ...feedback, good: feedback.good + 1, total: feedback.total + 1})
   }
 
   const handleNeutralClick = () => {
     setFeedback(
       {
         ...feedback, 
-        neutral: feedback.neutral + 1 
+        neutral: feedback.neutral + 1, 
+        total: feedback.total + 1 
       }
     )
   }
 
   const handleBadClick = () => {
-    setFeedback({ ...feedback, bad: feedback.bad + 1 })
+    setFeedback({ ...feedback, bad: feedback.bad + 1, total: feedback.total + 1})
   }
+
+  const calculateAverage = () => {
+    if (feedback.total > 0) {
+      return (feedback.good - feedback.bad) / feedback.total;
+    }
+    return 0;
+  };
+
+  const getAverageStatistic = () => {
+    return <Statistic text="average" value={calculateAverage().toFixed(2)} />;
+  };
+
+  const calculatePositivePercentage = () => {
+    if (feedback.total > 0) {
+      return (feedback.good / feedback.total) * 100;
+    }
+    return 0;
+  };
+
+  const getPositiveStatistic = () => {
+    return <Statistic text="positive" value={'%' + calculatePositivePercentage().toFixed(2)} />;
+  };
 
   return (
     <div>
@@ -35,6 +58,9 @@ const App = () => {
       <Statistic text="good" value={feedback.good} />
       <Statistic text="neutral" value={feedback.neutral} />
       <Statistic text="bad" value={feedback.bad} />
+      <Statistic text="all" value={feedback.total} />
+      {getAverageStatistic()}
+      {getPositiveStatistic()}
     </div>
   )
 }
