6a3436644a376d73607659e3f6678092f198b0ef
Yusuf <fuzzy.repeal-0v@icloud.com>
2024-06-26 01:13:39 +0300

    exercise 1.11 submission

--- part1/unicafe/App.jsx
+++ part1/unicafe/App.jsx
@@ -1,55 +1,62 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-const Display = ({ text }) => <h1>{text}</h1>;
-const Display2 = ({ text }) => <h2>{text}</h2>;
-const StatisticLine = ({ text, value }) => (<div> {text} {value} </div>);
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Display = ({ text }) => <h1>{text}</h1>
+const Display2 = ({ text }) => <h2>{text}</h2>
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = ({ feedback }) => {
   const calculateAverage = () => {
     if (feedback.total > 0) {
-      return (feedback.good - feedback.bad) / feedback.total;
+      return (feedback.good - feedback.bad) / feedback.total
     }
-    return 0;
-  };
+    return 0
+  }
 
   const calculatePositivePercentage = () => {
     if (feedback.total > 0) {
-      return (feedback.good / feedback.total) * 100;
+      return (feedback.good / feedback.total) * 100
     }
-    return 0;
-  };
+    return 0
+  }
 
   return (
-    <div>
-      <StatisticLine text="good" value={feedback.good} />
-      <StatisticLine text="neutral" value={feedback.neutral} />
-      <StatisticLine text="bad" value={feedback.bad} />
-      <StatisticLine text="all" value={feedback.total} />
-      <StatisticLine text="average" value={calculateAverage().toFixed(2)} />
-      <StatisticLine text="positive" value={'%' + calculatePositivePercentage().toFixed(2)} />
-    </div>
-  );
-};
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={feedback.good} />
+        <StatisticLine text="neutral" value={feedback.neutral} />
+        <StatisticLine text="bad" value={feedback.bad} />
+        <StatisticLine text="all" value={feedback.total} />
+        <StatisticLine text="average" value={calculateAverage().toFixed(2)} />
+        <StatisticLine text="positive" value={calculatePositivePercentage().toFixed(2) + " %"} />
+      </tbody>
+    </table>
+  )
+}
 
 const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0, total: 0 });
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0, total: 0 })
 
   const handleGoodClick = () => {
-    setFeedback({ ...feedback, good: feedback.good + 1, total: feedback.total + 1 });
-  };
+    setFeedback({ ...feedback, good: feedback.good + 1, total: feedback.total + 1 })
+  }
 
   const handleNeutralClick = () => {
     setFeedback({
       ...feedback,
       neutral: feedback.neutral + 1,
       total: feedback.total + 1
-    });
-  };
+    })
+  }
 
   const handleBadClick = () => {
-    setFeedback({ ...feedback, bad: feedback.bad + 1, total: feedback.total + 1 });
-  };
+    setFeedback({ ...feedback, bad: feedback.bad + 1, total: feedback.total + 1 })
+  }
 
   return (
     <div>
@@ -64,7 +71,7 @@ const App = () => {
         <div>No feedback given</div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
